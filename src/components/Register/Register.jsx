553a812194b0bf537e52df330bde94291b493cbe
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-10 05:02:03 +0600

    simple input field validation, error, and success message

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,37 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
+import { useState } from "react";
 
 const Register = () => {
+	const [registerError, setRegisterError] = useState("");
+	const [success, setSuccess] = useState("");
+
 	const handleRegister = (e) => {
 		e.preventDefault();
 		const email = e.target.email.value;
 		const password = e.target.password.value;
 		console.log(email, password);
+
+		// reset error
+		setRegisterError("");
+		setSuccess("");
+
+		if (password.length < 6) {
+			setRegisterError(
+				"Password should be at least 6 characters or longer"
+			);
+			return;
+		}
+
 		// create user
 		createUserWithEmailAndPassword(auth, email, password)
 			.then((result) => {
 				console.log(result.user);
+				setSuccess("User Created Successfully.");
 			})
 			.catch((error) => {
 				console.error(error);
+				setRegisterError(error.message);
 			});
 	};
 
@@ -28,6 +46,7 @@ const Register = () => {
 						name="email"
 						placeholder="Email Address"
 						id=""
+						required
 					/>
 					<br />
 					<input
@@ -36,6 +55,7 @@ const Register = () => {
 						name="password"
 						placeholder="password"
 						id=""
+						required
 					/>
 					<br />
 					<input
@@ -44,6 +64,10 @@ const Register = () => {
 						value="Register"
 					/>
 				</form>
+				{registerError && (
+					<p className="text-red-700">{registerError}</p>
+				)}
+				{success && <p className="text-green-600">{success}</p>}
 			</div>
 		</div>
 	);
